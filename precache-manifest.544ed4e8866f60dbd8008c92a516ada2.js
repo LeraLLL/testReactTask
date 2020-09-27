90a94aa0d8d33e31968930eb8f7bb18f03b2b0e5
Lera Larina <hokgj@mail.ru>
2020-09-28 00:37:07 +0300

    Updates

---
+++ precache-manifest.544ed4e8866f60dbd8008c92a516ada2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674a581d8de69e352f103125cee9caa3",
+    "url": "/testReactTask/index.html"
+  },
+  {
+    "revision": "44220a3d44175e8af0d0",
+    "url": "/testReactTask/static/css/4.75b59849.chunk.css"
+  },
+  {
+    "revision": "a8513631c3ee356aa022",
+    "url": "/testReactTask/static/css/main.81e49168.chunk.css"
+  },
+  {
+    "revision": "0e1fc35f0f6bfb4d6392",
+    "url": "/testReactTask/static/js/2.cafd0dd4.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/testReactTask/static/js/2.cafd0dd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5504ef5e3686676d1443",
+    "url": "/testReactTask/static/js/3.3f6b8aab.chunk.js"
+  },
+  {
+    "revision": "44220a3d44175e8af0d0",
+    "url": "/testReactTask/static/js/4.608ce176.chunk.js"
+  },
+  {
+    "revision": "6e223bdbfe40f1b18779",
+    "url": "/testReactTask/static/js/5.a650185d.chunk.js"
+  },
+  {
+    "revision": "8e7fa176b006150306288bd092a696c0",
+    "url": "/testReactTask/static/js/5.a650185d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "637314e50bf5c9f484ee",
+    "url": "/testReactTask/static/js/6.cf4e3988.chunk.js"
+  },
+  {
+    "revision": "b325e61cea38b9cef999",
+    "url": "/testReactTask/static/js/7.34e7c0e4.chunk.js"
+  },
+  {
+    "revision": "d6cf13f09e2d86098100",
+    "url": "/testReactTask/static/js/8.f8c94dac.chunk.js"
+  },
+  {
+    "revision": "a8513631c3ee356aa022",
+    "url": "/testReactTask/static/js/main.15776a3d.chunk.js"
+  },
+  {
+    "revision": "80e96603add4662b899a",
+    "url": "/testReactTask/static/js/runtime-main.01ddffa9.js"
+  }
+]);
